32dc3cef1ef5f1b999b4a8cc02be908ff8d2d5df
Sylvain Mathieu <sylvain.mathieu@gmail.com>
2023-03-04 18:21:07 +0000

    --wip-- [skip ci]

--- pong.py
+++ pong.py
@@ -9,18 +9,57 @@ surface = pygame.display.set_mode((screenWidth,screenHeight))
 clock = pygame.time.Clock()
 
 p1Colour = (251,0,0)
-p2Colour = (0,0,255)
+p2Colour = (0,0, 255)
+playerHeight = 80
+player1 = pygame.Rect(2, 100, 10, playerHeight)
+player2 = pygame.Rect(screenWidth - 12, 300, 10, playerHeight)
+
+playerSpeed = 5
+p1Speed = 0
+p2Speed = 0
 
 isRunning = True
 while isRunning:
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                p2Speed = playerSpeed * -1
+            if event.key == pygame.K_DOWN:
+                p2Speed = playerSpeed
+            if event.key == pygame.K_w:
+                p1Speed = playerSpeed * -1
+            if event.key == pygame.K_s:
+                p1Speed = playerSpeed
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                p2Speed = 0
+            if event.key == pygame.K_DOWN:
+                p2Speed = 0
+            if event.key == pygame.K_w:
+                p1Speed = 0
+            if event.key == pygame.K_s:
+                p1Speed = 0
+        elif event.type == pygame.QUIT:
             isRunning = False
     
     surface.fill((255,255,255))
 
-    pygame.draw.rect(surface, p1Colour, (2,100,10,80))
-    pygame.draw.rect(surface, p2Colour, (screenWidth - 12,300,10,80))
+    player1.y += p1Speed
+    player2.y += p2Speed
+
+    if player1.y <= 3:
+        player1.y = 3
+    if player1.y >= screenHeight - playerHeight - 3:
+        player1.y = screenHeight - playerHeight - 3
+
+    if player2.y <= 3:
+        player2.y = 3
+    if player2.y >= screenHeight - playerHeight - 3:
+        player2.y = screenHeight - playerHeight - 3
+
+    pygame.draw.rect(surface, p1Colour, player1)
+    pygame.draw.rect(surface, p2Colour, player2)
 
     pygame.display.flip()
     pygame.display.update()
